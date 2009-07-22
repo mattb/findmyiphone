6e0d7a33d6cdcc0c1196be220eb208026cb2d3d2
Matt Biddulph <matt@hackdiary.com>
2009-07-22 21:29:20 +0100

    Bump revision in gemspec to see if github will build my gem.

--- lib/findmyiphone.rb
+++ lib/findmyiphone.rb
@@ -3,7 +3,7 @@ require 'mechanize'
 require 'json'
 
 class FindMyIphone
-    VERSION = [0, 1, 0]
+    VERSION = [0, 1, 1]
     def initialize(username, password)
         @username = username
         @password = password
